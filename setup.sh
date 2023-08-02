a5061b4a7defa6fcce6f5a0d83c99e707b031dda
moisesmgft <moisesmgft@gmail.com>
2023-08-02 20:29:21 -0300

    setup done

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-#vcs import < src/ros2.repos src
+vcs import < src/dependencies.repos src
+vcs import < src/frtl_2023/dependencies.repos src
+vcs import < src/simulation/dependencies.repos src
 sudo apt-get update
 sudo apt remove ros-humble-ros-gz-bridge
 source /opt/ros/humble/setup.bash
 rosdep update
 rosdep install --from-paths src --rosdistro humble --ignore-src -y -i \
         --skip-keys="gz-transport12 gz-common5 gz-math7 gz-msgs9 gz-gui7 gz-cmake3 gz-sim7 Eigen3 opencv2 nlohmann_json qgroundcontrol"
-colcon build --event-handlers console_direct+ --cmake-args -DBUILD_TESTING=0
+colcon build
