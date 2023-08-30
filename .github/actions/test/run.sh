b024600fd98f62dddad57b249e2ac59b5481c922
moisesmgft <moisesmgft@gmail.com>
2023-08-30 23:50:49 +0000

    workflow configurado

--- .github/actions/test/run.sh
+++ .github/actions/test/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
-./tasks/simulation_less/setup.sh
+export GIT_CREDENTIALS=$GIT_CREDENTIALS
+git config --global credential.helper "store --file=/github/home/.git-credentials"
+echo "${GIT_CREDENTIALS}" > /github/home/.git-credentials
+
+./tasks/setup.sh
 ./tasks/build.sh
-./tasks/test.sh
+#./tasks/test.sh
